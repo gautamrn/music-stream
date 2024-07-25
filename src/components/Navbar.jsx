04b7a0faf2a09196b862a55ce6413451dc8c2cab
Gautam Nigam <gautamrn@umich.edu>
2024-07-25 15:03:18 -0400

    Added Featured Songs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,19 @@ const Navbar = () => {
     <>
         <div className='w-full flex justify-between items-center font-semibold'>
             <div className='flex items-center gap-2'>
-                <img className='w-8 bg-black p-2 rounded-2x1 cursor-pointer' src={assets.arrow_left} alt="" />
-                <img className='w-8 bg-black p-2 rounded-2x1 cursor-pointer' src={assets.arrow_right} alt="" />
+                <img className='w-8 bg-zinc-900 p-2 rounded-2x1 cursor-pointer' src={assets.arrow_left} alt="" />
+                <img className='w-8 bg-zinc-900 p-2 rounded-2x1 cursor-pointer' src={assets.arrow_right} alt="" />
             </div>
+
+            <div className='flex items-center gap-4'>
+              <p className='bg-pink-400 text-zinc-900 w-7 h-7 rounded-full flex items-center justify-center'>G</p>
+            </div>
+        </div>
+
+        <div className='flex items-center gap-2 mt-4'>
+          <p className='bg-white px-4 py-1 rounded-2xl cursor-pointer'>All</p>
+          <p className='bg-zinc-900 px-4 py-1 rounded 2xl cursor-pointer'>Music</p>
+          <p className='bg-zinc-900 px-4 py-1 rounded 2xl cursor-pointer'>Podcasts</p>
         </div>
     </>
   )
